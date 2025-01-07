edf0213782505fa976b52448964db61ffaba58fb
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-01-07 17:22:49 +0100

    Add test product entry

--- src/db/populate-db.sql
+++ src/db/populate-db.sql
@@ -1,2 +1,5 @@
 INSERT INTO `user` (username, email, password, name, surname) VALUES
 ('testuser', 'test@example.com', '$2y$10$eImiTXuWVxfM37uY4JANjQ8VVKf7A6xgZjwFqi/Nj3T4N0qP/a0z.', 'Gino', 'Pino');
+
+INSERT INTO product (`name`, price, stock, `description`, img) VALUES
+('PlayStation 5', 499.99, 10, '', 'prova.png');
