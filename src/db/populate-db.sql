774c842e4cc2dd268f98bd0d1cb417827e163416
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-01-07 11:50:48 +0100

    Add populate-db sql file

---
+++ src/db/populate-db.sql
@@ -0,0 +1,2 @@
+INSERT INTO `user` (username, email, password, name, surname) VALUES
+('testuser', 'test@example.com', '$2y$10$eImiTXuWVxfM37uY4JANjQ8VVKf7A6xgZjwFqi/Nj3T4N0qP/a0z.', 'Gino', 'Pino');
