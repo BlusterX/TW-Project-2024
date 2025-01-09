3f1f82d8ebe3cfe87dcddf97d48eaa5ba070f8b9
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-01-09 23:12:43 +0100

    Add shopping page

---
+++ src/shopping.php
@@ -0,0 +1,7 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "Shopping";
+$templateParams["nome"] = "template-shopping.php";
+require("template/base.php");
+?>
---
+++ src/template/template-shopping.php
@@ -0,0 +1,36 @@
+<!-- CONTENUTO PRINCIPALE -->
+        <main class="container py-4">
+            <!-- Intestazione colonne -->
+            <div class="row mb-2">
+                <div class="col-12 col-md-8 offset-md-2 d-flex justify-content-between px-5">
+                    <span class="fw-bold">Prodotto</span>
+                    <span class="fw-bold">Quantità</span>
+                    <span class="fw-bold">Prezzo</span>
+                </div>
+            </div>
+            <!-- 1 Prodotto -->
+            <div class="row mb-3">
+                <div class="col-12 col-md-8 offset-md-2">
+                    <div class="d-flex align-items-center justify-content-between p-4" style="background-color: #999; border-radius: 10px;">
+                        <!-- Immagine + nome -->
+                        <div class="d-flex flex-column align-items-center">
+                            <img src="img/prova.png" alt="Playstation 5" style="width:80px; height:auto;"/>
+                            <span class="mt-2 fw-bold">Playstation 5</span>
+                        </div>
+                        <div class="pe-4">
+                            <!-- Quantità -->
+                            <input type="number" class="form-control text-center" style="width:60px;" value="1" min="1"/>
+                        </div>
+                        <!-- Prezzo -->
+                        <span class="fs-5">500€</span>
+                    </div>
+                </div>
+            </div>
+            <!-- TOTALE -->
+            <div class="row mb-3">
+                <div class="col-12 col-md-8 offset-md-2 d-flex justify-content-end">
+                    <span class="fw-bold fs-5 me-2">TOTALE:</span>
+                    <span class="fs-5 fw-bold">500€</span>
+                </div>
+            </div>
+        </main>
