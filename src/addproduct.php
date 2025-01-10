c07108d447ea5f506839fa9f2e11d1a01ba96b51
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-01-10 11:14:31 +0100

    Add product addition functionality with form template

---
+++ src/addproduct.php
@@ -0,0 +1,7 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "Aggiunta-Prodotto";
+$templateParams["nome"] = "template-addprod.php";
+require("template/base.php");
+?>
---
+++ src/template/template-addprod.php
@@ -0,0 +1,33 @@
+<div class="container mt-5">
+    <h1 class="m-4">Aggiungi un nuovo prodotto</h1>
+    <form class="m-4" action="process-addprod.php" method="post" enctype="multipart/form-data">
+        <div class="form-group mb-3">
+            <label for="name">Nome del prodotto:</label>
+            <input type="text" class="form-control" id="name" name="name" required/>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="price">Prezzo:</label>
+            <input type="number" class="form-control" id="price" name="price" step="0.01" required/>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="description">Descrizione:</label>
+            <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="image">Immagine del prodotto:</label>
+            <input type="file" class="form-control-file" id="img" name="img" accept="img/*" required/>
+        </div>
+
+        <div class="form-group mb-3">
+            <label for="stock">Numero in stock:</label>
+            <input type="number" class="form-control" id="stock" name="stock" required/>
+        </div>
+
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Aggiungi Prodotto</button>
+        </div>      
+    </form>
+</div>
