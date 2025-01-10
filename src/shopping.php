65bd9df64b62a6a32067949889f95c303079391d
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-01-10 11:42:41 +0100

    Fix display products dynamically

--- src/shopping.php
+++ src/shopping.php
@@ -3,5 +3,6 @@ require_once("bootstrap.php");
 
 $templateParams["titolo"] = "Shopping";
 $templateParams["nome"] = "template-shopping.php";
+$templateParams["products"] = $dbh->getAllProducts();
 require("template/base.php");
 ?>
--- src/template/template-shopping.php
+++ src/template/template-shopping.php
@@ -8,24 +8,25 @@
                     <span class="fw-bold">Prezzo</span>
                 </div>
             </div>
-            <!-- 1 Prodotto -->
+            <?php foreach ($templateParams["products"] as $product): ?>
             <div class="row mb-3">
                 <div class="col-12 col-md-8 offset-md-2">
                     <div class="d-flex align-items-center justify-content-between p-4" style="background-color: #999; border-radius: 10px;">
                         <!-- Immagine + nome -->
                         <div class="d-flex flex-column align-items-center">
-                            <img src="<?php echo UPLOAD_DIR . "prova.png" ; ?>" alt="Playstation 5" style="max-width: 150px;"/>
-                            <span class="mt-2 fw-bold">Playstation 5</span>
+                            <img class="img-card-custom" src="<?php echo UPLOAD_DIR . $product["img"]; ?>" alt="<?php echo $product["name"]; ?>"/>
+                            <span class="mt-2 fw-bold"><?php echo $product["name"]; ?></span>
                         </div>
                         <div class="pe-4">
                             <!-- Quantità -->
                             <input type="number" class="form-control text-center" style="width:60px;" value="1" min="1"/>
                         </div>
                         <!-- Prezzo -->
-                        <span class="fs-5">500€</span>
+                        <span class="fs-5">€<?php echo $product["price"]; ?></span>
                     </div>
                 </div>
             </div>
+            <?php endforeach; ?>
             <!-- TOTALE -->
             <div class="row mb-3">
                 <div class="col-12 col-md-8 offset-md-2 d-flex justify-content-end">
@@ -33,4 +34,5 @@
                     <span class="fs-5 fw-bold">500€</span>
                 </div>
             </div>
+            
         </main>
