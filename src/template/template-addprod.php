fae66718ab50d614bfaa7d8de9e340053265854d
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-01-10 12:19:19 +0100

    Fix

--- src/template/template-addprod.php
+++ src/template/template-addprod.php
@@ -1,6 +1,6 @@
 <div class="container mt-5">
     <h1 class="m-4">Aggiungi un nuovo prodotto</h1>
-    <form class="m-4" action="process-addprod.php" method="post" enctype="multipart/form-data">
+    <form class="m-4" method="post" enctype="multipart/form-data">
         <div class="form-group mb-3">
             <label for="name">Nome del prodotto:</label>
             <input type="text" class="form-control" id="name" name="name" required/>
