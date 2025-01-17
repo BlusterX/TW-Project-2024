f366fec188130f93bf3f5a7c32629db814485fde
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-01-17 12:38:23 +0100

    Fix product param

--- src/shopping.php
+++ src/shopping.php
@@ -3,6 +3,6 @@ require_once("bootstrap.php");
 
 $templateParams["titolo"] = "Shopping";
 $templateParams["nome"] = "template-shopping.php";
-$templateParams["products"] = $dbh->getAllProducts();
+$templateParams["products"] = $dbh->getCartProducts(getUserId());
 require("template/base.php");
 ?>
