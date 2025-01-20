2d652ce9a1323deb274dedb3caeb9b3d038c201c
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-01-20 10:29:46 +0100

    Add login check for cart page

--- src/shopping.php
+++ src/shopping.php
@@ -1,6 +1,10 @@
 <?php
 require_once("bootstrap.php");
 
+if(!isUserLoggedIn()) {
+    header("Location: login.php");
+}
+
 $templateParams["titolo"] = "Shopping";
 $templateParams["nome"] = "template-shopping.php";
 $templateParams["products"] = $dbh->getCartProducts(getUserId());
