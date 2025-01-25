9570c1c2de2c02ff2c13751977c19039f99c9858
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-01-25 18:04:26 +0100

    Fix comments

--- src/your-orders.php
+++ src/your-orders.php
@@ -5,26 +5,27 @@ if(!isUserLoggedIn()) {
     header("Location: login.php");
 }
 
+// An order has been confirmed and user has completed the payment
 if(isset($_GET["order_id"])) {
     $idOrder = $_GET["order_id"];
-    $dbh->generateShippingDate($idOrder);
-    //Notifica per l'utente
+    $dbh->generateShippingDate($idOrder); // Assign the shipping date
+
+    // Notify the user that the order has been confirmed
     $notificationTitle = "L'ordine #" . $idOrder . " è in spedizione";
     $products = $dbh->getOrderedProducts($idOrder);
-
     $notificationMessage = "Prodotti ordinati:";
     foreach($products as $product) {
         $notificationMessage .= "<br/>" . $product["name"] . " x" . $product["quantity"];
     }
     $dbh->createNotification(getUserId(), $notificationTitle, $notificationMessage);
 
-    //Notifica per l'admin della conferma dell'acquisto
+    // Notify the admin that a user has placed the order
     $username = $dbh->getUsernameById(getUserId());
     $notificationTitle = "L'ordine #" . $idOrder . " è stato effettuato da " . $username;
     $admin_id = $dbh->getAdminId(); // The site has only one admin
     $dbh->createNotification($admin_id, $notificationTitle, $notificationMessage);
 
-    //Notifica per l'admin se un prodotto è esaurito
+    // Notify the admin if a product is no longer available
     $allProductSoldOut = $dbh->getProductSoldOut();
     foreach($products as $product) {
         foreach($allProductSoldOut as $productSoldOut) {
