f0af1a3902ede241c1ff6c73f0c883adda92f57c
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-01-26 22:15:30 +0100

    Fix db

--- src/db/populate-db.sql
+++ src/db/populate-db.sql
@@ -22,7 +22,7 @@ INSERT INTO product (`name`, price, stock, `description`, img) VALUES
 ('1996 Nintendo Console', 349.99, 5, 'La console Nintendo del 1996 è un classico senza tempo, perfetta per i collezionisti e gli appassionati di retrogaming.', '1996_nintendo_console.png'),
 ('Nes Classic Edition', 99.99, 15, 'La NES Classic Edition ripropone i grandi classici Nintendo con un design retrò e giochi preinstallati.', 'nes_classic_edition.png'),
 ('Nintendo 3DS Xl', 249.99, 8, 'Nintendo 3DS XL offre un display più grande e un’immersiva esperienza di gioco portatile, compatibile con tutti i giochi 3DS.', 'nintendo_3ds_xl.png'),
-('Nintendo Switch blue and red Controllers', 299.99, 12, 'La Nintendo Switch con Joy-Con blu e rosso offre un’esperienza di gioco versatile e coinvolgente, perfetta per ogni occasione.', 'nintendo_switch_br_controllers.png'),
+('Nintendo Switch', 299.99, 12, 'La Nintendo Switch con Joy-Con blu e rosso offre un’esperienza di gioco versatile e coinvolgente, perfetta per ogni occasione.', 'nintendo_switch_br_controllers.png'),
 ('Nintendo Switch Lite', 199.99, 10, 'Nintendo Switch Lite è la console portatile ideale per chi ama giocare ovunque con leggerezza e comodità.', 'nintendo_switch_lite.png'),
 ('Nintendo Wii U', 259.99, 7, 'Nintendo Wii U unisce il divertimento della console tradizionale con funzionalità uniche del GamePad.', 'nintendo_wii_u.png'),
 ('Playstation PS3', 149.99, 20, 'Playstation 3 rivoluziona il gaming con grafica HD, giochi indimenticabili e funzionalità multimediali.', 'playstation_ps3.png'),
