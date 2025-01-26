3c918a5260f1ce1df636b8fbbcfc991d913b1d5f
Gjovalin Mujaj <gjovalin.mujaj@studio.unibo.it>
2025-01-26 19:12:00 +0100

    Fix

--- src/template/template-notifications.php
+++ src/template/template-notifications.php
@@ -1,4 +1,4 @@
-<header class="bg-dark text-white text-center py-3">
+<header class="bg-dark text-white text-center py-4">
     <h1>Notifiche</h1>
 </header>
 <?php if (empty($templateParams["notifiche"])) { ?>
